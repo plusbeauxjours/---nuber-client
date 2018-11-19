fabbe4c3e7abd04a5db675b403aae0ab26dd735a
plusbeauxjours <plusbeauxjours@gmail.com>
2018-11-19 12:10:27 +0100

    #2.14.1 BackArrow CSS & Input CSS & PhoneLogin CSS

--- src/Routes/Login/LoginPresenter.tsx
+++ src/Routes/Login/LoginPresenter.tsx
@@ -1,34 +1,35 @@
-import React from 'react';
-import { RouteComponentProps } from 'react-router';
-import styled from '../../typed-components';
-import bgImage from '../../images/bg.png';
-import Helmet from 'react-helmet';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { RouteComponentProps } from "react-router";
+import styled from "../../typed-components";
+import bgImage from "../../images/bg.png";
+import Helmet from "react-helmet";
+import { Link } from "react-router-dom";
 
 const Container = styled.div`
-    height: 100vh;
+  height: 100vh;
 `;
 
 const Header = styled.header`
-    height: 70%;
-    background: linear-gradient(rgba(0, 153, 196, 0.5), rgba(0, 153, 196, 0.4)),
-        url(${bgImage});
-    display: flex;
-    align-items: center;
-    justify-content: center;
+  height: 70%;
+  background: linear-gradient(rgba(0, 153, 196, 0.5), rgba(0, 153, 196, 0.4)),
+    url(${bgImage});
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 const Logo = styled.div`
-    width: 110px;
-    height: 110px;
-    background-color: white;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    box-shadow: 0 14px 28px rgba(0, 0, 0, 0/25), 0 -14px 28px rgba(0, 0, 0,0 0.22);
-    text-transform: uppercase;
-    font-weight: 500px;
-    font-size: 25px;
+  width: 110px;
+  height: 110px;
+  background-color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  box-shadow: 0 14px 28px rgba(0, 0, 0, 0/25),
+    0 -14px 28px rgba(0, 0, 0, 0 0.22);
+  text-transform: uppercase;
+  font-weight: 500px;
+  font-size: 25px;
 `;
 
 const Title = styled.h1``;
@@ -36,64 +37,64 @@ const Title = styled.h1``;
 const Footer = styled.div``;
 
 const Subtitle = styled.h2`
-    font-size: 30px;
+  font-size: 30px;
 `;
 
 const FakeInput = styled.div`
-    margin: 50px, 0px;
-    font-size: 25px;
-    font-weight: 300;
+  margin: 50px, 0px;
+  font-size: 25px;
+  font-weight: 300;
 `;
 
 const PhoneLogin = styled.div`
-    padding: 20px;
-    cursor: default;
+  padding: 20px;
+  cursor: pointer;
 `;
 
 const Grey = styled.span`
-    color: ${props => props.theme.greyColor};
-    margin-left: 10px;
+  color: ${props => props.theme.greyColor};
+  margin-left: 10px;
 `;
 
 const SocialLogin = styled.div`
-    border-top: 1px solid ${props => props.theme.greyColor};
-    padding: 30px 20px;
+  border-top: 1px solid ${props => props.theme.greyColor};
+  padding: 30px 20px;
 `;
 
 const SocialLink = styled.span`
-    color: ${props => props.theme.blueColor};
-    font-size: 20px;
-    cursor: default;
+  color: ${props => props.theme.blueColor};
+  font-size: 20px;
+  cursor: pointer;
 `;
 
 interface IProps extends RouteComponentProps<any> {}
 
 const LoginPresenter: React.SFC<IProps> = () => (
-    <Container>
-        <Helmet>
-            <title>Login | Nuber</title>
-        </Helmet>
-        <Header>
-            <Logo>
-                <Title>Nuber</Title>
-            </Logo>
-        </Header>
-        <Footer>
-            <Link to={"/phone-login"}>
-                    <PhoneLogin>
-                    <Subtitle>Get moving with Nuber</Subtitle>
-                    <FakeInput>
-                        🇰🇷 +82<Grey>Enter your mobile number</Grey>
-                    </FakeInput>
-                </PhoneLogin>
-            </Link>
-            <SocialLogin>
-                <Link to={"/social-login"}>
-                    <SocialLink>Or connect with social</SocialLink>
-                </Link>
-            </SocialLogin>
-        </Footer>
-    </Container>
+  <Container>
+    <Helmet>
+      <title>Login | Nuber</title>
+    </Helmet>
+    <Header>
+      <Logo>
+        <Title>Nuber</Title>
+      </Logo>
+    </Header>
+    <Footer>
+      <Link to={"/phone-login"}>
+        <PhoneLogin>
+          <Subtitle>Get moving with Nuber</Subtitle>
+          <FakeInput>
+            🇰🇷 +82<Grey>Enter your mobile number</Grey>
+          </FakeInput>
+        </PhoneLogin>
+      </Link>
+      <SocialLogin>
+        <Link to={"/social-login"}>
+          <SocialLink>Or connect with social</SocialLink>
+        </Link>
+      </SocialLogin>
+    </Footer>
+  </Container>
 );
 
-export default LoginPresenter; 
+export default LoginPresenter;
