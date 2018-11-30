16a9755e64edcd2a8b0d1188317776a401c1717f
plusbeauxjours <plusbeauxjours@gmail.com>
2018-11-30 14:13:08 +0100

    #2.44 AddPlace Mutation

--- src/Routes/AddPlace/AddPlaceContainer.tsx
+++ src/Routes/AddPlace/AddPlaceContainer.tsx
@@ -1,27 +1,66 @@
 import React from "react";
 import { RouteComponentProps } from "react-router-dom";
 import AddPlacePresenter from "./AddPlacePresenter";
+import { addPlaceVariables, addPlace } from "../../types/api";
+import { Mutation } from "react-apollo";
+import { ADD_PLACE } from "./AddPlaceQueries";
+import { toast } from "react-toastify";
+import { GET_PLACES } from "../../sharedQueries";
 
 interface IState {
   address: string;
+  lat: number;
+  lng: number;
   name: string;
 }
 
 interface IProps extends RouteComponentProps<any> {}
 
+class AddPlaceQuery extends Mutation<addPlace, addPlaceVariables> {}
+
 class AddPlaceContainer extends React.Component<IProps, IState> {
   public state = {
     address: "",
+    lat: 1.34,
+    lng: 1.34,
     name: ""
   };
   public render() {
-    const { address, name } = this.state;
+    const { address, name, lat, lng } = this.state;
+    const { history } = this.props;
     return (
-      <AddPlacePresenter
-        onInputChange={this.onInputChange}
-        address={address}
-        name={name}
-      />
+      <AddPlaceQuery
+        mutation={ADD_PLACE}
+        onCompleted={data => {
+          const { AddPlace } = data;
+          if (AddPlace.ok) {
+            toast.success("Place added!");
+            setTimeout(() => {
+              history.push("/places");
+            }, 2000);
+          } else {
+            toast.error(AddPlace.error);
+          }
+        }}
+        refetchQueries={[{ query: GET_PLACES }]}
+        variables={{
+          address,
+          isFav: false,
+          lat,
+          lng,
+          name
+        }}
+      >
+        {(addPlaceFn, { loading }) => (
+          <AddPlacePresenter
+            onInputChange={this.onInputChange}
+            address={address}
+            name={name}
+            loading={loading}
+            onSubmit={addPlaceFn}
+          />
+        )}
+      </AddPlaceQuery>
     );
   }
 
--- src/Routes/AddPlace/AddPlacePresenter.tsx
+++ src/Routes/AddPlace/AddPlacePresenter.tsx
@@ -5,7 +5,8 @@ import Header from "src/Components/Header";
 import Helmet from "react-helmet";
 import Form from "src/Components/Form";
 import Input from "../../Components/Input/Input";
-// import Button from "src/Components/Button";
+import { MutationFn } from "react-apollo";
+import Button from "src/Components/Button";
 
 const Container = styled.div`
   padding: 0 40px;
@@ -25,12 +26,16 @@ interface IProps {
   address: string;
   name: string;
   onInputChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  loading;
+  onSubmit: MutationFn;
 }
 
 const AddPlacePresenter: React.SFC<IProps> = ({
   address,
   name,
-  onInputChange
+  onInputChange,
+  loading,
+  onSubmit
 }) => (
   <React.Fragment>
     <Helmet>
@@ -38,21 +43,23 @@ const AddPlacePresenter: React.SFC<IProps> = ({
     </Helmet>
     <Header title={"Add Place"} backTo={"/"} />
     <Container>
-      <Form submitFn={null}>
+      <Form submitFn={onSubmit}>
         <ExtendedInput
           placeholder={"Name"}
           type={"text"}
           onChange={onInputChange}
           value={name}
+          name={"name"}
         />
         <ExtendedInput
           placeholder={"Address"}
           type={"text"}
           onChange={onInputChange}
           value={address}
+          name={"address"}
         />
         <ExtendedLink to={"/find-address"}>Pick place from map</ExtendedLink>
-        {/* <Button onClick={null} value={loading ? "Adding place" : "Add Place"} /> */}
+        <Button onClick={null} value={loading ? "Adding place" : "Add Place"} />
       </Form>
     </Container>
   </React.Fragment>
