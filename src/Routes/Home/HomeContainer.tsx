a8e7a946967c0b08a9f7073ad1369287fe6c102f
plusbeauxjours <plusbeauxjours@gmail.com>
2018-12-15 20:48:39 +0700

    #2.72 Ride Screen part One

--- src/Routes/Home/HomeContainer.tsx
+++ src/Routes/Home/HomeContainer.tsx
@@ -26,6 +26,7 @@ import {
 } from "../../types/api";
 import { SubscribeToMoreOptions } from "apollo-client";
 import { SUBSCRIBE_NEARBY_RIDES, ACCEPT_RIDE } from "./HomeQueries";
+import { acceptRide } from "../../types/api";
 
 interface IState {
   isMenuOpen: boolean;
@@ -144,7 +145,10 @@ class HomeContainer extends React.Component<IProps, IState> {
                         subscribeToMore(rideSubscriptionOptions);
                       }
                       return (
-                        <AcceptRide mutation={ACCEPT_RIDE}>
+                        <AcceptRide
+                          mutation={ACCEPT_RIDE}
+                          onCompleted={this.handleRideAcceptance}
+                        >
                           {acceptRideFn => (
                             <HomePresenter
                               loading={loading}
@@ -399,9 +403,11 @@ class HomeContainer extends React.Component<IProps, IState> {
     }
   };
   public handleRideRequest = (data: requestRide) => {
+    const { history } = this.props;
     const { RequestRide } = data;
     if (RequestRide.ok) {
       toast.success("Drivce requested, finding a driver");
+      history.push(`/ride/${RequestRide.ride!.id}`);
     } else {
       toast.error(RequestRide.error);
     }
@@ -415,6 +421,13 @@ class HomeContainer extends React.Component<IProps, IState> {
       this.setState({
         isDriving
       });
+    }
+  };
+  public handleRideAcceptance = (data: acceptRide) => {
+    const { history } = this.props;
+    const { UpdateRideStatus } = data;
+    if (UpdateRideStatus.ok) {
+      history.push(`/ride/${UpdateRideStatus.rideId}`);
     }
   };
 }
