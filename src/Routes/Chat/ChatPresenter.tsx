e84966f6a35bf4bcc1dd467f5c2762191c067865
plusbeauxjours <plusbeauxjours@gmail.com>
2018-12-18 15:47:04 +0700

    #2.77 Chat Screen Query

--- src/Routes/Chat/ChatPresenter.tsx
+++ src/Routes/Chat/ChatPresenter.tsx
@@ -1,12 +1,41 @@
 import React from "react";
 import styled from "../../typed-components";
 import Header from "../../Components/Header/index";
+import { getChat, userProfile } from "../../types/api";
+import Message from "../../Components/Message/Message";
 
 const Container = styled.div``;
 
-const ChatPresenter: React.SFC = () => (
+interface IProps {
+  data?: getChat;
+  userData?: userProfile;
+  loading: boolean;
+}
+
+const ChatPresenter: React.SFC<IProps> = ({
+  data: { GetChat: { chat = null } = {} } = {},
+  userData: { GetMyProfile: { user = null } = {} } = {},
+  loading
+}) => (
   <Container>
     <Header title={"Chat"} />
+    {!loading && chat && user && (
+      <React.Fragment>
+        {chat.messages &&
+          chat.messages.map(message => {
+            if (message) {
+              return (
+                <Message
+                  key={message.id}
+                  text={message.text}
+                  mine={user.id === message.userId}
+                />
+              );
+            }
+            return null;
+          })}
+      </React.Fragment>
+    )}
   </Container>
 );
 
